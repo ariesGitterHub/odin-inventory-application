00046e8750a08cc21dfd9d8b47807eb888c0ccb8
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2026-01-20 13:45:29 -0500

    testing changes in db/pool.js to see if a prod only set up will resolve Render issue

--- db/pool.js
+++ db/pool.js
@@ -1,31 +1,40 @@
 // POOL MANAGES YOUR CONNECTIONS
 
-require("dotenv").config(); // Load environment variables
+// require("dotenv").config(); // Load environment variables
 
-// Below is the "Pool — a managed pool of clients"; Think of Pool as having a set of reserved tables at a restaurant. The pool keeps several connections open in the background. When your app needs to run a query, it borrows a connection from the pool. When the query finishes, the connection goes back into the pool for reuse.
+// // Below is the "Pool — a managed pool of clients"; Think of Pool as having a set of reserved tables at a restaurant. The pool keeps several connections open in the background. When your app needs to run a query, it borrows a connection from the pool. When the query finishes, the connection goes back into the pool for reuse.
 
+// const { Pool } = require("pg");
+
+// let pool;
+
+// if (process.env.DATABASE_URL) {
+//   // Production / Render / Neon
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: { rejectUnauthorized: false },
+//   });
+// } else {
+//   // Local development
+//   pool = new Pool({
+//     host: process.env.PG_HOST || "localhost",
+//     user: process.env.PG_USER,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     port: Number(process.env.PG_PORT) || 5432,
+//     ssl: false,
+//   });
+// }
+
+// module.exports = pool;
+
+// TEXT for Render
+
+require("dotenv").config();
 const { Pool } = require("pg");
 
-let pool;
-
-if (process.env.DATABASE_URL) {
-  // Production / Render / Neon
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-  });
-} else {
-  // Local development
-  pool = new Pool({
-    host: process.env.PG_HOST || "localhost",
-    user: process.env.PG_USER,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: Number(process.env.PG_PORT) || 5432,
-    ssl: false,
-  });
-}
-
-module.exports = pool;
-
+module.exports = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
